2166ebeb951469c76359dec644865231997e04f7
David Code Howard <me@davidcode.ca>
2024-09-03 17:50:10 -0400

    Add flake file
    Flake file can be used by nix users to install the correct version of
    gcc and start a development shell with `nix develop`.

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "Use gcc14 to support C++23 print function";
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        gcc14
+      ];
+    };
+  };
+}
